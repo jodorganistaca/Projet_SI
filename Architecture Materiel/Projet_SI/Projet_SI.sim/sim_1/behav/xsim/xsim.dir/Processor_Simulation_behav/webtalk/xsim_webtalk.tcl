21e954078ffb60d1899c5f76d3d0f6047b581b8c
jodorganistaca <jodorganistaca@unal.edu.co>
2022-06-02 18:17:33 +0200

    add readme

--- Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/xsim_webtalk.tcl
+++ Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/organist/Documents/repo/Projet_SI/Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Jun  2 15:03:10 2022" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Jun  2 18:06:27 2022" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "33fc8f5b-ec27-4809-a723-e1021e45e56d" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "1cbb3f61-2c91-4433-99fd-46bc730199ac" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "2" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 20.04.4 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-8700 CPU @ 3.20GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "122704_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4086263179 -regid "" -xml /home/organist/Documents/repo/Projet_SI/Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/organist/Documents/repo/Projet_SI/Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/organist/Documents/repo/Projet_SI/Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 3128122852 -regid "" -xml /home/organist/Documents/repo/Projet_SI/Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/organist/Documents/repo/Projet_SI/Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/organist/Documents/repo/Projet_SI/Architecture Materiel/Projet_SI/Projet_SI.sim/sim_1/behav/xsim/xsim.dir/Processor_Simulation_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Projet_SI
+
+## About The Project
+The compiler is the program that allows the creation of an object code from a source code. It is necessary when using a high level language and allows to implement more and more functions and basic operations of a programming language. In this project we have made the different links to transform a C code to a VHDL operation reading for our Basys 3 FPGA board. We implemented our C compiler by syntactic and semantic analysis in Lex and Yacc, then writing in assembly, secondly the interpreters and cross-assembler developed and thirdly the implementation in VHDL of the operations of the cross-assembler for the application at physical level.
+
+
+### Built With
+
+
+* [Lex](http://dinosaur.compilertools.net/lex/index.html)
+* [Yacc](http://dinosaur.compilertools.net/yacc/index.html)
+* [VHDL-Vivado-XILINX](https://www.xilinx.com/products/design-tools/vivado.html)
+
+## Getting Started
+```sh
+git clone https://github.com/jodorganistaca/Projet_SI
+```
+### Prerequisites
+
+Vivado gcc
+
+## Usage
+To create the Compiler with the lex and yacc file run:
+```sh
+make 
+```
+This will generate an file Compiler then you can run differents tests in the folder "test" there are 
+```sh
+Complier < ./test/tx.c 
+```
+This will generate an file assembleur.asm in the folder output to interpretate this file you can run
+```sh
+make interpretate 
+```
+And finally to transform the output into an acceptable input to the microprocessor you can run 
+```sh
+make cross 
+```
+This will generate an file in hexa in the folder output 
+
+## Contact
+
+Kezia Sorlin - [@Keziahsorlin](https://github.com/Keziahsorlin) - sorlin@insa-toulouse.fr
+
+Organista Jose - [@jodorganistaca](https://github.com/jodorganistaca) - organist@insa-toulouse.fr
